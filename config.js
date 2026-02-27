54238a79a20005a0c63a86af18f26193c3495c44
Kevin Luvizotto <kevin.luvizotto@hotmail.com>
2026-02-27 12:10:45 -0300

    Updated config.js for AKS

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 window.APP_CONFIG = {
-  IDENTITY_BASE_URL: "http://localhost:5084",
-  TELEMETRY_BASE_URL: "http://localhost:5026",
-  ALERTS_BASE_URL: "http://localhost:5049",
-  PROPERTIES_BASE_URL: "http://localhost:5035",
-  LOGIN_PATH: "/auth/login"
+  IDENTITY_BASE_URL: "/identity",
+  PROPERTIES_BASE_URL: "/properties",
+  TELEMETRY_BASE_URL: "/telemetry",
+  ALERTS_BASE_URL: "/alerts"
 };
